15812c785fbfa8c3fcc60014c3e9151d6343b143
Rafel Bover Lladó <rlladobover@gmail.com>
2021-10-25 12:46:05 +0200

    Entregable_3
    controles de escala

---
+++ Entregable 3/Assets/Script/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScaleGameObject(new Vector3(0.5f, 0f, 0f), KeyCode.X);
+        ScaleGameObject(new Vector3(0f, 0.5f, 0f), KeyCode.Y);
+        ScaleGameObject(new Vector3(0f, 0f, 0.5f), KeyCode.Z);
+    }
+
+    public void ScaleGameObject(Vector3 Scale, KeyCode kCode)
+    {
+        if (Input.GetKeyDown(kCode))
+        {
+            transform.localScale += Scale;
+        }
+    }
+}
